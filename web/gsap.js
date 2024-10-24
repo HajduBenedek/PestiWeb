3e13fc5ecd97acec2f4717a2bce4b1b6656334b0
Hajdu Benedek <benedek1104@gmail.com>
2024-10-24 02:44:19 +0200

    dark mode shadow, content rész reszponzivitás

--- web/gsap.js
+++ web/gsap.js
@@ -15,4 +15,23 @@ HeaderTL
     .from(bannerImg, { y: -50, opacity: 0, duration: 0.5 })
     .from(bannerH1, { y: -20, opacity: 0, duration: 0.5 }, "-=0.35")
     .from(bannerParagh, { y: -20, opacity: 0, duration: 0.5 }, "-=0.35")
-    .to(bannerBtn, { y: -20, opacity: 1, duration: 0.2 }, "-=0.5")
+    .to(bannerBtn, { opacity: 1, duration: 0.2 }, "-=0.5")
+
+
+const SectionServices = document.querySelector(".Section-Services");
+const ServicesTitle = SectionServices.querySelector(".Section-Title-Container h1");
+const ServicesUnderline = SectionServices.querySelector(".Section-Title-Container .underline");
+
+const ServicesContentHelper = document.getElementById("ServicesContent");
+const ServicesList = ServicesContentHelper.querySelectorAll(".card");
+
+const ServicesTL = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".Section-Services"
+    }
+})
+
+ServicesTL
+    .from(ServicesTitle, { y: -50, opacity: 0, duration: 0.5 })
+    .from(ServicesUnderline, { width: 0, opacity: 0, duration: 0.5 })
+//.from(ServicesList[0], { y: -50, opacity: 0, duration: 0.5 })
